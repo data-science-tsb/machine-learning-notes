e168e1a88ce05398b95792b7e7ea9f2c07424d4d
Lyndon B <hack.of.all.codes@gmail.com>
2021-11-07 05:45:33 +0000

    inital attempt at training with sagemaker estimators - failing at loading the training files for some reason i am yet to discover

--- 01_End2End_ML/training-script.py
+++ 01_End2End_ML/training-script.py
@@ -1,4 +1,11 @@
-from sklearn.externals import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import joblib
 import argparse
 import os
 
@@ -20,3 +27,34 @@ if __name__ =='__main__':
     args, _ = parser.parse_known_args()
 
     # ... load from args.train and args.test, train a model, write model to args.model_dir.
+    
+    training_set = pd.read_csv(args.train)
+    housing_features = training_set.drop('median_house_value', axis=1)
+    housing_labels = training_set['median_house_value'].copy()
+
+    housing_num = housing_features.drop('ocean_proximity', axis=1) #we can only scale numberic data
+
+    num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy='median')),
+        ('std_scaler', StandardScaler()),
+    ])
+
+    housing_num_tr = num_pipeline.fit_transform(housing_num)
+    
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import OneHotEncoder
+
+    num_attribs = list(housing_num)
+    cat_attribs = ['ocean_proximity']
+
+    full_pipeline = ColumnTransformer([
+        ('num', num_pipeline, num_attribs),
+        ('cat', OneHotEncoder(), cat_attribs),
+    ])
+
+    housing_prepared = full_pipeline.fit_transform(housing_features)
+    
+    lin_reg = LinearRegression()
+    lin_reg.fit(housing_prepared, housing_labels)
+    
+    joblib.dump(lin_reg, os.path.join(args.model_dir, "model.joblib"))
